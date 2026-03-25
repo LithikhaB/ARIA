b87345d1790e3997eed3a84b20d99e17bd5c538a
shirleyclaire <shirleyclaire.2404@gmail.comH>
2026-03-26 04:16:49 +0530

    changed base in config files

--- clarix/vite.config.ts
+++ clarix/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/clarix/',
+    base: '/ARIA/clarix/'
 
     plugins: [react(), tailwindcss()],
 
